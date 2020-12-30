bb1ca25a3a99253f19a3dc046f88688a50d4d7b4
pqhtobi <52424594+pqhtobi@users.noreply.github.com>
2020-12-30 22:28:26 +0700

    Create register_form.php

---
+++ online-shopping-system-advanced-master/register_form.php
@@ -0,0 +1,81 @@
+    <div class="wait overlay">
+        <div class="loader"></div>
+    </div>
+    <style>
+    .input-borders{
+        border-radius:30px;
+    }
+    </style>
+				<!-- row -->
+				
+                <div class="container-fluid">
+					
+						
+						
+						<!-- /Billing Details -->
+						
+								<form id="signup_form" onsubmit="return false" class="login100-form">
+									<div class="billing-details jumbotron">
+                                    <div class="section-title">
+                                        <h2 class="login100-form-title p-b-49" >ĐĂNG KÍ </h2>
+                                    </div>
+                                    <div class="form-group ">
+                                    
+                                        <input class="input form-control input-borders" type="text" name="f_name" id="f_name" placeholder="TÊN">
+                                    </div>
+                                    <div class="form-group">
+                                    
+                                        <input class="input form-control input-borders" type="text" name="l_name" id="l_name" placeholder="HỌ">
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="input form-control input-borders" type="email" name="email"  placeholder="Email">
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="input form-control input-borders" type="password" name="password" id="password" placeholder="MẬT KHẨU">
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="input form-control input-borders" type="password" name="repassword" id="repassword" placeholder="NHẬP LẠI MẬT KHẨU">
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="input form-control input-borders" type="text" name="mobile" id="mobile" placeholder="SỐ ĐIỆN THOẠI">
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="input form-control input-borders" type="text" name="address1" id="address1" placeholder="ĐỊA CHỈ">
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="input form-control input-borders" type="text" name="address2" id="address2" placeholder="THÀNH PHỐ">
+                                    </div>
+                                    
+                                    
+                                    <div style="form-group">
+                                       <input class="primary-btn btn-block"  value="ĐĂNG KÝ" type="submit" name="signup_button">
+                                    </div>
+                                    <div class="text-pad">
+                                    <a href="" data-toggle="modal" data-target="#Modal_login">ĐĂNG NHẬP</a>
+                                       
+                                    </div>
+                                    
+                                
+								</form>
+                                <div class="login-marg">
+						<!-- Billing Details -->
+						<div class="row">
+                            <div class="col-md-2"></div>
+                            <div class="col-md-8" id="signup_msg">
+                                    
+
+                                </div>
+                                <!--Alert from signup form-->
+                            </div>
+                            <div class="col-md-2"></div>
+                        </div>
+
+						
+					</div>
+                    </div> 
+
+					
+				
+				<!-- /row -->
+
+			
