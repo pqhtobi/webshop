f58e68f70556501d57781593d38e992ac8c91121
pqhtobi <52424594+pqhtobi@users.noreply.github.com>
2020-12-30 22:28:55 +0700

    Create newslettter.php

---
+++ online-shopping-system-advanced-master/newslettter.php
@@ -0,0 +1,41 @@
+
+
+        
+
+        <div id="newsletter" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+					
+						<div class="newsletter">
+							<p>LIÊN HỆ VỚI CHÚNG TÔI  <strong>GÓP Ý VỀ SHOP</strong></p>
+							<form id="offer_form" onsubmit="return false">
+								<input class="input" type="email" id="email" name="email" placeholder="Enter Your Email">
+								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> GÓP Ý</button>
+							</form>
+							<div class="" id="offer_msg">
+                                <!--Alert from signup form-->
+                            </div>
+							<ul class="newsletter-follow">
+								<li>
+									<a href="https://www.facebook.com/PQHtobi/"><i class="fa fa-facebook"></i></a>
+								</li>
+								<li>
+									<a href="https://www.youtube.com/channel/UCUx-Kw6XlMg0Ye4EozQqbLA"><i class="fa fa-twitter"></i></a>
+								</li>
+								<li>
+									<a href="https://www.instagram.com/p.q.h.tobi/?hl=vi"><i class="fa fa-instagram"></i></a>
+								</li>
+								<li>
+									<a href="https://github.com/pqhtobi"><i class="fa fa-github"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
